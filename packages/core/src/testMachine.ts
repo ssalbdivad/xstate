8783ca4d8b52fa8ccbeee280a1e9051d1086897c
David Blass <david@arktype.io>
2023-06-19 11:03:45 -0400

    some cleanup from gist

--- packages/core/src/testMachine.ts
+++ packages/core/src/testMachine.ts
@@ -1,6 +1,8 @@
-type Compute<A extends any> = { [K in keyof A]: A[K] } & unknown;
+type Compute<A> = { [K in keyof A]: A[K] } & unknown;
 
-// should create an object mapping the name of each state to the names of the events of that state
+// should create an object mapping the name of each state to another object
+// mapping the names of the states it leads to to the event names that lead to
+// those states.
 type SourceEvents<States extends StatesDefinition> = Compute<
   {
     [K in keyof States]: {
@@ -14,6 +16,84 @@ type ExtractSourceEvents<States, TEvent, U> = Compute<{
     ? Extract<TEvent, { type: EventType }>
     : never;
 }>;
+
+type ExtractEventsByAction<
+  States extends StatesDefinition,
+  Extracted
+> = Compute<
+  {
+    [K in keyof States]: States[K]['entry'] extends string
+      ? { [_ in States[K]['entry']]: Extracted[K & keyof Extracted] }
+      : never;
+  }[keyof States]
+>;
+
+type ParseStates<TStates extends StatesDefinition, TEvent> = Compute<{
+  events: ExtractSourceEvents<TStates, TEvent, SourceEvents<TStates>>;
+  actions: ExtractEventsByAction<
+    TStates,
+    ExtractSourceEvents<TStates, TEvent, SourceEvents<TStates>>
+  >;
+}>;
+
+type Conform<T, Base> = T extends Base ? T : Base;
+
+type EventsByState = { [stateName: Key]: unknown };
+
+type EventsByAction = { [actionName: Key]: unknown };
+
+type ParsedStates = {
+  events: EventsByState;
+  actions: EventsByAction;
+};
+
+type Key = string | symbol | number;
+
+type StatesDefinition = Record<Key, State<Key, unknown>>;
+
+type State<Name, Event> = {
+  on: Record<string, Name>;
+  entry?: string | ((event: Event) => void);
+};
+
+type ValidateState<TState, Name, Event> = {
+  [K in keyof TState]: K extends 'on' | 'entry'
+    ? Conform<TState[K], State<Name, Event>[K]>
+    : TState[K];
+};
+
+interface Machine<TEvent extends { type: string }> {
+  // initial: string;
+  context: {
+    events: TEvent;
+  };
+  states: unknown;
+}
+
+function createMachine<
+  TEvent extends { type: string },
+  const TConfig,
+  const TStates,
+  /** @ts-expect-error nede to figure out how to constrain this without breaking inference */
+  Parsed extends ParsedStates = ParseStates<TStates, TEvent>
+>(
+  config: TConfig & { types: { events: TEvent } } & {
+    states: {
+      [K in keyof TStates]: ValidateState<
+        TStates[K],
+        keyof TStates,
+        Parsed['events'][K]
+      >;
+    };
+  },
+  implementations: {
+    actions: {
+      [K in keyof Parsed['actions']]: (event: Parsed['actions'][K]) => void;
+    };
+  }
+) {
+  return {} as Parsed;
+}
 
 const result = createMachine(
   //    ^?
@@ -35,15 +115,33 @@ const result = createMachine(
           NEXT: 'b',
           MOAR: 'c'
         }
+        // states: {
+        //   a1: {
+        //     on: {
+        //       NEXT: 'a2'
+        //     }
+        //   },
+        //   a2: {}
+        // }
       },
       b: {
+        entry: (event) => {
+          //     ^?
+          event;
+        },
         on: {
           NEXT: 'c'
         }
       },
       c: {
+        initial: 'c1',
         entry: 'doStuff',
         on: {}
+        // states: {
+        //   c1: {
+        //     entry: 'doInitialStuff'
+        //   }
+        // }
       }
     }
   },
@@ -56,104 +154,3 @@ const result = createMachine(
     }
   }
 );
-
-type ExtractEventsByState<TStates extends StatesDefinition, TEvent> = Compute<{
-  [Name in keyof TStates]: Extract<
-    TEvent,
-    { type: SourceEvents<TStates>[Name] }
-  >;
-}>;
-
-type ExtractEventsByAction<
-  States extends StatesDefinition,
-  Extracted
-> = Compute<
-  {
-    [K in keyof States]: States[K]['entry'] extends string
-      ? { [_ in States[K]['entry']]: Extracted[K & keyof Extracted] }
-      : never;
-  }[keyof States]
->;
-
-type ParseStates<TStates extends StatesDefinition, TEvent> = Compute<{
-  extracted: ExtractSourceEvents<TStates, TEvent, SourceEvents<TStates>>;
-  sources: SourceEvents<TStates>;
-  actions: ExtractEventsByAction<
-    TStates,
-    ExtractSourceEvents<TStates, TEvent, SourceEvents<TStates>>
-  >;
-  events: ExtractEventsByState<TStates, TEvent>;
-}>;
-
-type Conform<T, Base> = T extends Base ? T : Base;
-
-type EventsByState = { [stateName: Key]: unknown };
-
-type EventsByAction = { [actionName: Key]: unknown };
-
-type ParsedStates = {
-  events: EventsByState;
-  actions: EventsByAction;
-};
-
-type Key = string | symbol | number;
-
-type StatesDefinition = Record<Key, State<Key, unknown>>;
-
-type State<Name, Event> = {
-  on: Record<string, Name>;
-  event?: Event;
-  entry?: string | ((event: Event) => void);
-};
-
-type ValidateState<TState, Name, Event> = {
-  [K in keyof TState]: TState[K] extends (...args: unknown[]) => any
-    ? (event: Event) => void
-    : Conform<TState[K], State<Name, Event>[K]>;
-};
-
-interface Machine<TEvent extends { type: string }> {
-  // initial: string;
-  context: {
-    events: TEvent;
-  };
-  states: unknown;
-}
-
-function createMachine<
-  TEvent extends { type: string },
-  const TConfig,
-  const TStates,
-  Parsed extends ParsedStates = ParseStates<TStates, TEvent>
->(
-  config: TConfig & { types: { events: TEvent } } & {
-    states: {
-      [K in keyof TStates]: ValidateState<
-        TStates[K],
-        keyof TStates,
-        Parsed['events'][K]
-      >;
-    };
-  },
-  implementations: {
-    actions: {
-      [K in keyof Parsed['actions']]: (event: Parsed['actions'][K]) => void;
-    };
-  }
-  //  InternalMachineImplementations<
-  //   TConfig['context'],
-  //   TConfig['context']['event'],
-  //   ResolveTypegenMeta<
-  //     TypegenDisabled,
-  //     TConfig['context']['event'],
-  //     ParameterizedObject,
-  //     ActorMap
-  //   >
-  // >
-) {
-  return {} as Parsed;
-  // return new StateMachine<any, any, any, any, any>(
-  //   config,
-  //   implementations as any
-  // );
-}
